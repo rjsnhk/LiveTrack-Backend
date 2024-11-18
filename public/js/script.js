f22b8f81d3ae7dae8b024fd7daa05db182a18f6e
rjsnhk <nahakrajesh003@gmail.com>
2024-11-19 01:58:51 +0530

    SECOND

---
+++ public/js/script.js
@@ -0,0 +1,43 @@
+const socket = io();
+
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      socket.emit("send-location", { latitude, longitude });
+    },
+    (error) => {
+      console.log(error);
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    }
+  );
+}
+
+const map = L.map("map").setView([0, 0], 16);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://rjsnhk.onrender.com">RJSNHK</a> contributors',
+}).addTo(map);
+
+const markers = {};
+
+socket.on("receive-location", (data) => {
+  const { id, latitude, longitude } = data;
+  map.setView([latitude, longitude]);
+  if (!markers[id]) {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+  } else {
+    markers[id].setLatLng([latitude, longitude]);
+  }
+});
+
+socket.on("user-disconnected", (id) => {
+  if(markers[id]){
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
+});
